1000c1676aebbe80945818ab5fd403aceaa1313c
shane garrity <shanedgarrity@gmail.com>
2019-05-05 21:05:11 -0400

    connect to postgres db

--- django_pi/settings.py
+++ django_pi/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'django_pi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_db',
+        'USER': 'django_user',
+        'PASSWORD': 'django-password',
+        'HOST': 'localhost'
     }
 }
 
@@ -107,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/New_York'
 
 USE_I18N = True
 
